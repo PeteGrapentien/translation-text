c3256f5be6a43868f3ccfcbf46fc9a7d1fa6de87
Peter Grapentien <a100933@qh623x3nf2.prog1.com>
2024-10-03 10:17:35 -0400

    Added base project

---
+++ translation-text/translation-text/translation_textApp.swift
@@ -0,0 +1,17 @@
+//
+//  translation_textApp.swift
+//  translation-text
+//
+//  Created by Peter Grapentien on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct translation_textApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
